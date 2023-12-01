8666940a26e2d863cc4339359c2a8b88843aafee
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2023-12-02 00:23:39 +0200

    Create BaseAnimal.java

---
+++ src/zoo/entities/animals/BaseAnimal.java
@@ -0,0 +1,66 @@
+package zoo.entities.animals;
+
+import zoo.common.ExceptionMessages;
+
+public abstract class BaseAnimal implements Animal{
+    private String name;
+    private String kind;
+    private double kg;
+    private double price;
+
+    public BaseAnimal(String name, String kind, double kg, double price) {
+        this.setName(name);
+        this.setKind(kind);
+        this.setKg(kg);
+        this.setPrice(price);
+    }
+    public void setName(String name) {
+        if(name == null || name.equals("")){
+            throw new NullPointerException(ExceptionMessages.ANIMAL_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        if(kind == null || kind.equals("")){
+            throw new NullPointerException(ExceptionMessages.ANIMAL_KIND_NULL_OR_EMPTY);
+        }
+        this.kind = kind;
+    }
+
+    @Override
+    public double getKg() {
+        return this.kg;
+    }
+
+    public void setKg(double kg) {
+        this.kg = kg;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    public void setPrice(double price) {
+        if(price <= 0){
+            throw new IllegalArgumentException(ExceptionMessages.ANIMAL_PRICE_BELOW_OR_EQUAL_ZERO);
+        }
+        this.price = price;
+    }
+
+    @Override
+    public abstract void eat();
+}
+
+
+}
