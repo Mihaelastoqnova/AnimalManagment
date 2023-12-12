15524888f287d1c44670991b45a02ef1968e1f3b
Mihaelastoqnova <122827141+Mihaelastoqnova@users.noreply.github.com>
2023-12-12 22:14:24 +0200

    Create BaseArea.java

---
+++ src/zoo/entities/areas/BaseArea.java
@@ -0,0 +1,97 @@
+package zoo.entities.areas;
+
+import zoo.common.ExceptionMessages;
+import zoo.entities.animals.Animal;
+import zoo.entities.foods.Food;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class BaseArea implements Area {
+    private String name;
+    private int capacity;
+    private Collection<Food> foods;
+    private Collection<Animal> animals;
+
+    public BaseArea(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.foods = new ArrayList<>(); //?
+        this.animals = new ArrayList<>(); //?
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.equals("")) {
+            throw new NullPointerException(ExceptionMessages.AREA_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public Collection<Animal> getAnimals() {
+        return this.animals;
+    }
+
+    @Override
+    public Collection<Food> getFoods() {
+        return this.foods;
+    }
+
+    @Override
+    public int sumCalories() {
+        int sum = 0;
+        for (Food food : this.foods) {
+            sum += food.getCalories();
+        }
+        return sum;
+    }
+
+    @Override
+    public void addAnimal(Animal animal) {
+        if (this.animals.size() >= capacity) {
+            throw new IllegalStateException(ExceptionMessages.NOT_ENOUGH_CAPACITY);
+        }
+        this.animals.add(animal);
+    }
+
+    @Override
+    public void removeAnimal(Animal animal) {
+        this.animals.remove(animal);
+    }
+
+    @Override
+    public void addFood(Food food) {
+        this.foods.add(food);
+    }
+
+    @Override
+    public void feed() {
+        this.animals.forEach(Animal::eat);
+    }
+
+    @Override
+    public String getInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s (%s):", this.name, this.getClass().getSimpleName()));
+        sb.append(System.lineSeparator());
+        sb.append("Animals: ");
+        if(this.animals.isEmpty()){
+            sb.append("None");
+        } else {
+            sb.append(this.animals.stream().map(Animal::getName).collect(Collectors.joining(" ")));
+        }
+        sb.append(System.lineSeparator());
+        sb.append("Foods: ");
+        sb.append(this.foods.size());
+        sb.append(System.lineSeparator());
+        sb.append("Calories: ");
+        sb.append(this.sumCalories());
+        return sb.toString().trim();
+    }
+}
